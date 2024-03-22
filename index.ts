078fb1ec76149cdbfbd5160aa5bc642448573c6a
MuhammadHasan77 <mrmuhammadhasanattari@gmail.com>
2024-03-23 01:00:03 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter your First number", type: "number", name: "FirstNumber" },
+  {
+    message: "Enter your Second number",
+    type: "number",
+    name: "SecondNumber",
+  },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+// conditional statement
+if (answer.operator === "Addition") {
+  console.log( answer.FirstNumber + answer.SecondNumber);
+} else if (answer.operator === "Subtraction") {
+    console.log( answer.FirstNumber - answer.SecondNumber);
+  } else if (answer.operator === "Multiplication") {
+    console.log( answer.FirstNumber * answer.SecondNumber);
+  } else if (answer.operator === "Division") {
+    console.log( answer.FirstNumber / answer.SecondNumber);
+  } else {
+    console.log("Please valid operator")
+  }
